cc890744babd2240e56a5897147ca2718cbd985c
Sumedha <pramods@umich.edu>
2014-03-08 23:30:23 -0500

    Updated Databse to pg fully

--- app/controllers/calendar_controller.rb
+++
@@ -1,6 +0,0 @@
-class CalendarController < ApplicationController
-  def show
-    @date = params[:date] ? Date.parse(params[:date]) : Date.today
-    @events_by_date = Event.group(@date.)
-  end
-end
